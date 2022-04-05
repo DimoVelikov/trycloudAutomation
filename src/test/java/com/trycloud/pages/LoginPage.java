19547e2d3903be6bb1f9f482a8554c9f2e502889
alimu0223 <alimu0223@outlook.com>
2022-04-05 14:18:28 -0400

    added login method and extended BasePage

--- src/test/java/com/trycloud/pages/LoginPage.java
+++ src/test/java/com/trycloud/pages/LoginPage.java
@@ -1,15 +1,18 @@
 package com.trycloud.pages;
 
+import com.trycloud.utilities.ConfigurationReader;
 import com.trycloud.utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class LoginPage {
+public class LoginPage extends BasePage{
 
+/*
     public LoginPage(){
         PageFactory.initElements(Driver.getDriver(),this);
     }
+*/
 
     @FindBy(id = "user")
     public WebElement usernameInput;
@@ -22,5 +25,14 @@ public class LoginPage {
 
     @FindBy(xpath = "//p[@class='warning wrongPasswordMsg']")
     public WebElement wrongUsernameOrPassword;
+
+
+    public void Login(){
+        Driver.getDriver().get(ConfigurationReader.getProperty("url"));
+        usernameInput.sendKeys(ConfigurationReader.getProperty("username"));
+        passwordInput.sendKeys(ConfigurationReader.getProperty("password"));
+        loginBtn.click();
+    }
+
 
 }
